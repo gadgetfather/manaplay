83f1e6010d4297b5af6a338fc316ee3396e7e33a
Akshay Kamble <kamblea635@gmail.com>
2022-04-07 14:59:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Navbar, Sidebar } from "./components/index";
+import { HomePage } from "./pages";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="page_container">
+      <Navbar />
+      <Sidebar />
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
