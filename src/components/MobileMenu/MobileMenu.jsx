a2e1e0858bcf7a827588dcdaf39d2e2ee404d6cc
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 01:19:13 +0530

    add Navlink

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,20 +1,49 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import * as styles from "./MobileMenu.module.css";
 export function MobileMenu() {
   return (
     <div className={styles.mobile_menu}>
-      <span title="Home" className="material-icons-outlined">
-        home
-      </span>
-      <span title="Playlist" className="material-icons-outlined">
-        playlist_play
-      </span>
-      <span title="History" className="material-icons-outlined">
-        history
-      </span>
-      <span title="Liked videos" className="material-icons">
-        thumb_up_off_alt
-      </span>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `link ${styles.active}` : "link"
+        }
+        to={"/"}
+      >
+        <span title="Home" className="material-icons-outlined">
+          home
+        </span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `link ${styles.active}` : "link"
+        }
+        to={"/playlist"}
+      >
+        <span title="Playlist" className="material-icons-outlined">
+          playlist_play
+        </span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `link ${styles.active}` : "link"
+        }
+        to={"/history"}
+      >
+        <span title="History" className="material-icons-outlined">
+          history
+        </span>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? `link ${styles.active}` : "link"
+        }
+        to={"/liked-videos"}
+      >
+        <span title="Liked videos" className="material-icons">
+          thumb_up_off_alt
+        </span>
+      </NavLink>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,50 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import * as styles from "./Sidebar.module.css";
 export function Sidebar() {
   return (
     <aside className={`${styles.sidebar}`}>
       <div className={styles.sidebar_actions}>
-        <span title="Home" className="material-icons-outlined">
-          home
-        </span>
-        <span title="Playlist" className="material-icons-outlined">
-          playlist_play
-        </span>
-        <span title="History" className="material-icons-outlined">
-          history
-        </span>
-        <span title="Liked videos" className="material-icons">
-          thumb_up_off_alt
-        </span>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `link ${styles.active}` : "link"
+          }
+          to={"/"}
+        >
+          <span title="Home" className="material-icons-outlined">
+            home
+          </span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `link ${styles.active}` : "link"
+          }
+          to={"/playlist"}
+        >
+          <span title="Playlist" className="material-icons-outlined">
+            playlist_play
+          </span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `link ${styles.active}` : "link"
+          }
+          to={"/history"}
+        >
+          <span title="History" className="material-icons-outlined">
+            history
+          </span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `link ${styles.active}` : "link"
+          }
+          to={"/liked-videos"}
+        >
+          <span title="Liked videos" className="material-icons">
+            thumb_up_off_alt
+          </span>
+        </NavLink>
       </div>
     </aside>
   );
