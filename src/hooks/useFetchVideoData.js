388335b2bff91a17e201f705e2e1cda614ec9443
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 15:13:28 +0530

    added loader

--- src/hooks/useFetchVideoData.js
+++ src/hooks/useFetchVideoData.js
@@ -2,11 +2,14 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 export const useFetchVideoData = () => {
   const [videos, setvideos] = useState([]);
+  const [loader, setLoader] = useState(false);
   useEffect(
     () =>
       (async () => {
         try {
+          setLoader(true);
           const response = await axios.get("/api/videos");
+          setLoader(false);
           const {
             data: { videos },
           } = response;
@@ -17,5 +20,5 @@ export const useFetchVideoData = () => {
       })(),
     []
   );
-  return videos;
+  return { videos, loader };
 };
