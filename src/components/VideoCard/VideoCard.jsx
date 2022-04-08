45da03828499f3fe516d232566b26b109051b896
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 12:20:38 +0530

    update video card according to data

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as styles from "./VideoCard.module.css";
-export function VideoCard() {
+export function VideoCard(props) {
+  const { creator_image, creator, title, views, _id, thumbnail } = props;
   const navigate = useNavigate();
   const handleVideo = () => {
     navigate("/watch");
@@ -9,22 +10,18 @@ export function VideoCard() {
   return (
     <div onClick={handleVideo} className={styles.video_card_container}>
       <div className={styles.video_thumbnail}>
-        <img
-          className={styles.thumbnail}
-          src="https://i.ytimg.com/vi/WfduJifpOB8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC6HbJ-MDaxrr7vAFx5SLmMogra_g"
-          alt=""
-        />
+        <img className={styles.thumbnail} src={thumbnail} alt="video-art" />
       </div>
       <div className={styles.details}>
         <img
           className={styles.creator_image}
-          src="https://yt3.ggpht.com/ytc/AKedOLSN0ta03ELPkWsoHGw5ZM0jxtEegOPXOcROrWxMig=s68-c-k-c0x00ffffff-no-rj"
+          src={creator_image}
           alt="creator"
         />
         <div className={styles.video_description}>
-          <span className={styles.video_title}>goodbye olympus</span>
-          <span className={styles.creator_name}>aceu </span>
-          <span className={styles.view_count}>212K views</span>
+          <span className={styles.video_title}>{title}</span>
+          <span className={styles.creator_name}>{creator} </span>
+          <span className={styles.view_count}>{views} views</span>
         </div>
       </div>
     </div>
