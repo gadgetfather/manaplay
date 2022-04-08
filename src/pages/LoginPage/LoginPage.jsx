8beedcf0d59261ea01fb699e9980a507900bf45d
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 15:46:09 +0530

    add : create Login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 import * as styles from "./LoginPage.module.css";
 export function LoginPage() {
   return (
-    <main className={styles.main_content_login}>
-      <form className={styles.form_container}>
-        <label htmlFor="email">Email</label>
-        <input type="email" required />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <button className={`btn btn-primary ${styles.submit_btn}`}>
-          Login
-        </button>
-        <Link className="link" to="/sdss">
-          Dont Have account?
-        </Link>
-      </form>
-    </main>
+    <>
+      <main className={styles.main_content_login}>
+        <h1 className={styles.page_title}>Login</h1>
+        <form className={styles.form_container}>
+          <label htmlFor="email">Email</label>
+          <input type="email" required />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+          <button className={`btn btn-primary ${styles.submit_btn}`}>
+            Login
+          </button>
+          <Link className="link" to="/sdss">
+            Dont Have account?
+          </Link>
+        </form>
+      </main>
+    </>
   );
 }
