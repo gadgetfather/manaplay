1f8282d892261c05104c67ed71570a3c844c8ba6
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 12:20:11 +0530

    video listing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { VideoCard } from "../../components";
+import { useFetchVideoData } from "../../hooks/useFetchVideoData";
 import * as styles from "./HomePage.module.css";
 export function HomePage() {
+  const videos = useFetchVideoData();
+
   return (
     <main className={styles.main_content_home}>
       <div className={styles.category_pills_container}>
@@ -10,12 +13,9 @@ export function HomePage() {
         <span className={styles.category_pills}>Sports</span>
       </div>
       <div className={styles.video_grid}>
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        {videos.map((video) => (
+          <VideoCard key={video._id} {...video} />
+        ))}
       </div>
     </main>
   );
