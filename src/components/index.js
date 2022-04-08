137d422dc9fbe371903817b983908d39541ad173
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 10:41:37 +0530

    add export for similarVideoCard

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar/Navbar";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { MobileMenu } from "./MobileMenu/MobileMenu";
+export { SimilarVideoCard } from "./SimilarVideoCard/SimilarVideoCard";
