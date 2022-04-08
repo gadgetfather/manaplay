b175fb2d12e61d126ad9583a5949b8c52435e404
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 15:56:56 +0530

    update path for signin button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as styles from "./Navbar.module.css";
 export function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <h1 className={styles.brand_name}>Manaplay</h1>
+      <Link className="link" to={"/"}>
+        <h1 className={styles.brand_name}>Manaplay</h1>
+      </Link>
       <div className={styles.nav_actions}>
         <span className={`${styles.icons} material-icons-outlined`}>
           search
         </span>
-        <button className={`btn btn-secondary ${styles.sign_in}`}>
+        <Link to={"/login"} className={`btn btn-secondary ${styles.sign_in}`}>
           Sign In
-        </button>
+        </Link>
         {/* <span className={`${styles.icons} material-icons-outlined`}>
           person
         </span> */}
