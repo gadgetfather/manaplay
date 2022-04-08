cc8f70155711046a45c3e3e7aacc553f33ece32f
Akshay Kamble <kamblea635@gmail.com>
2022-04-08 10:45:49 +0530

    update route for singleVideoPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MobileMenu, Navbar, Sidebar } from "./components/index";
-import { HomePage } from "./pages";
+import { HomePage, SingleVideoPage } from "./pages";
 
 function App() {
   return (
@@ -12,10 +12,11 @@ function App() {
       <Routes>
         <Route path="*" element={<h1>This under construction</h1>} />
         <Route path="/" element={<HomePage />} />
-        <Route path="watch-later" element={<h1>Watch later</h1>} />
-        <Route path="liked-videos" element={<h1>Liked videos</h1>} />
-        <Route path="history" element={<h1>History</h1>} />
-        <Route path="playlist" element={<h1>Playlist</h1>} />
+        <Route path="/watch-later" element={<h1>Watch later</h1>} />
+        <Route path="/liked-videos" element={<h1>Liked videos</h1>} />
+        <Route path="/history" element={<h1>History</h1>} />
+        <Route path="/playlist" element={<h1>Playlist</h1>} />
+        <Route path="/watch" element={<SingleVideoPage />} />
       </Routes>
     </div>
   );
