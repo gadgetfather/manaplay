ac86cc9552481b403981778b6fd20968f52fcd05
Akshay Kamble <kamblea635@gmail.com>
2022-04-10 19:31:45 +0530

    add export for likedPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from "./HomePage/HomePage";
 export { SingleVideoPage } from "./SingleVideoPage/SingleVideoPage";
 export { LoginPage } from "./LoginPage/LoginPage";
 export { SignupPage } from "./SignupPage/SignupPage";
+export { LikedPage } from "./LikedPage/LikedPage";
