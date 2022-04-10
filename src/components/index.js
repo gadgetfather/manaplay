577694ed6e0d607d81fcd6828c27d73c7282332f
Akshay Kamble <kamblea635@gmail.com>
2022-04-10 19:31:55 +0530

    add export for likedCard

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { VideoCard } from "./VideoCard/VideoCard";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { MobileMenu } from "./MobileMenu/MobileMenu";
 export { SimilarVideoCard } from "./SimilarVideoCard/SimilarVideoCard";
+export { LikedCard } from "./LikedCard/LikedCard";
