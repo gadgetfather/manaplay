e82d9417960ef83da63fb42fc10b357b8cf09718
Akshay Kamble <kamblea635@gmail.com>
2022-04-15 16:23:26 +0530

    add a modal

--- src/App.js
+++ src/App.js
@@ -1,66 +1,85 @@
 import { Route, Routes } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { MobileMenu, Navbar, Sidebar } from "./components/index";
+import { MobileMenu, Navbar, PlaylistModal, Sidebar } from "./components/index";
 import {
   HistoryPage,
   HomePage,
   LikedPage,
   LoginPage,
+  PlaylistPage,
   SignupPage,
   SingleVideoPage,
   WatchlaterPage,
+  SinglePlaylistPage,
 } from "./pages";
 import { ProtectedRoute } from "./route/ProtectedRoute";
 import MockmanEs from "mockman-js";
+import { usePlaylist } from "./context/playlist-context";
 
 function App() {
+  const {
+    playlists: { showModal },
+  } = usePlaylist();
   return (
-    <div className="page_container">
-      <Navbar />
-      <Sidebar />
-      <MobileMenu />
-      <Routes>
-        <Route path="/mock" element={<MockmanEs />} />
-        <Route path="*" element={<h1>This under construction</h1>} />
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/watch-later"
-          element={
-            <ProtectedRoute>
-              <WatchlaterPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/liked-videos"
-          element={
-            <ProtectedRoute>
-              <LikedPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <HistoryPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/playlist"
-          element={
-            <ProtectedRoute>
-              <h1>Playlist</h1>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/watch/:videoId" element={<SingleVideoPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </div>
+    <>
+      <div className="page_container">
+        <Navbar />
+        <Sidebar />
+        <MobileMenu />
+        <Routes>
+          <Route path="/mock" element={<MockmanEs />} />
+          <Route path="*" element={<h1>This under construction</h1>} />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/watch-later"
+            element={
+              <ProtectedRoute>
+                <WatchlaterPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/liked-videos"
+            element={
+              <ProtectedRoute>
+                <LikedPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <HistoryPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/playlist"
+            element={
+              <ProtectedRoute>
+                <PlaylistPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/watch/:videoId"
+            element={
+              <SingleVideoPage>
+                {showModal ? <PlaylistModal /> : ""}
+              </SingleVideoPage>
+            }
+          />
+          <Route
+            path="/playlist/:playlistId"
+            element={<SinglePlaylistPage />}
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
