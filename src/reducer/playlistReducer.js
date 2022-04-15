f70ba71192ddff20aa4b9d35fc73f1c37895a98d
Akshay Kamble <kamblea635@gmail.com>
2022-04-15 16:20:17 +0530

    create playlistReducer

---
+++ src/reducer/playlistReducer.js
@@ -0,0 +1,27 @@
+function updateArr(playlistArr, response) {
+  Object.assign(
+    playlistArr.find((item) => item._id === response.data.playlist._id),
+    response.data.playlist
+  );
+  return playlistArr;
+}
+
+export const playlistReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_PLAYLISTS":
+      return { ...state, playlistsArr: action.payload };
+    case "SHOW_MODAL":
+      return { ...state, showModal: true };
+    case "HIDE_MODAL":
+      return { ...state, showModal: false };
+    case "UPDATE_PLAYLIST":
+      return {
+        ...state,
+        playlistsArr: updateArr(state.playlistsArr, action.payload),
+      };
+    case "SET_SINGLE_PLAYLIST":
+      return { ...state, singlePlaylist: action.payload };
+    default:
+      break;
+  }
+};
