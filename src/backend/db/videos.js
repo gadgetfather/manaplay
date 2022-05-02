b9e4862f0ea99630f580af4278c013438e2e0400
Akshay Kamble <kamblea635@gmail.com>
2022-05-02 19:00:18 +0530

    add extra videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,6 +4,7 @@
  * */
 
 export const videos = [
+  // Aceu apex
   {
     _id: "WfduJifpOB8",
     title: "goodbye olympus",
@@ -15,7 +16,9 @@ export const videos = [
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
     views: "280K",
     creator: "aceu",
+    category: "apex",
   },
+  // aceu apex
   {
     _id: "ikhcIcWK7t4",
     title: "the ranked win streak",
@@ -27,7 +30,9 @@ export const videos = [
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
     views: "382K",
     creator: "aceu",
+    category: "apex",
   },
+  // aceu apex
   {
     _id: "y-NIHYgfgto",
     title: "they fear my wattson",
@@ -39,7 +44,36 @@ export const videos = [
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
     views: "292K",
     creator: "aceu",
+    category: "apex",
   },
+  {
+    _id: "arNbe7z7eoA",
+    title: "DROPPING 30 BOMBS ONLY IN RANKED!!",
+    creator_image:
+      "https://yt3.ggpht.com/KKs_hxBnuQMXN1xd5qO5VCyDQjsxtDfjZrs_RfujxI_tOWJlIob5_hxVAAPcD7pqbznRWSWfKw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/arNbe7z7eoA/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCaOm4KAkLNGazk0EA468cwuIro3A",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: "92K",
+    creator: "TSMImperialHal",
+    category: "apex",
+  },
+  {
+    _id: "i_KlIWzA_tA",
+    title: "apex ranked... but elegant",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLSN0ta03ELPkWsoHGw5ZM0jxtEegOPXOcROrWxMig=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/i_KlIWzA_tA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhsQsfIbP_D75hFDZBJTZOK8olXg",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: "160K",
+    creator: "aceu",
+    category: "apex",
+  },
+  // -------------------Trailers------------
+  // trailer ac valhalla
   {
     _id: "ssrNcwxALS4",
     title: "Assassin's Creed Valhalla - Official Trailer",
@@ -51,7 +85,48 @@ export const videos = [
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
     views: "17M",
     creator: "IGN",
+    category: "trailer",
   },
+  {
+    _id: "EE-4GvjKcfs",
+    title: "God Of War Ragnarok Reveal Trailer",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLSyRIOTnFGuNAWmraaNw-SQuv5r3HeRvlpRqGJe3cc=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/EE-4GvjKcfs/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA58hYO3Zy1sEVdM3IWMIXzf5z8LA",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: "13M",
+    creator: "Playstation",
+    category: "trailer",
+  },
+  {
+    _id: "qIQ3xNqkVC4",
+    title: "Marvel's Spider-Man 2 Reveal Trailer",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLSyRIOTnFGuNAWmraaNw-SQuv5r3HeRvlpRqGJe3cc=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/qIQ3xNqkVC4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCEqmPcRyNc3ovgDgBJV3pGcxn4yA",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: "23M",
+    creator: "Playstation",
+    category: "trailer",
+  },
+  {
+    _id: "-IJuKT1mHO8",
+    title: "Far Cry 6 - Official Reveal Trailer",
+    creator_image:
+      "https://yt3.ggpht.com/H_---ano_f27DOCejDhUdBMtBcxcv32MoXmFKQc_mBcxYSH4HPZTGuBUdm7UgI5VlutMOVX2=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/-IJuKT1mHO8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCE_22YGkeIpLe1AWyVXx6J0aOd3Q",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    views: "13M",
+    creator: "IGN",
+    category: "trailer",
+  },
+  // trailer battlefield
   {
     _id: "FDuMEodqXi0",
     title: "BATTLEFIELD 2042 Trailer",
@@ -63,5 +138,75 @@ export const videos = [
       "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
     views: "17M",
     creator: "Battlefield",
+    category: "trailer",
+  },
+  // lemon GTA V
+  {
+    _id: "kjHVDncXE70",
+    title: "GTA 5: PC Funny Moments",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLQ-2seCcUJOc3EUhNkfDwaZjls-FIzXYdy9UJszrw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/kjHVDncXE70/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC73fBYPdOCSepdnHWELSAmdlFpVQ",
+    description:
+      "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
+    views: "3.9M",
+    creator: "TheGamingLemon",
+    category: "GTA V",
+  },
+  // lemon gta v
+  {
+    _id: "Q8nVTwSSC9M",
+    title: "GTA 5: A Day In The Life of Michael!",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLQ-2seCcUJOc3EUhNkfDwaZjls-FIzXYdy9UJszrw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/Q8nVTwSSC9M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAKZ5p33pWXK7AqGQcdqAuOTxFvjg",
+    description:
+      "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
+    views: "6.8M",
+    creator: "TheGamingLemon",
+    category: "GTA V",
+  },
+  // mod GTA V
+  {
+    _id: "YrRlwUw0GY4",
+    title: "GTA 5: POV Ultra Realistic",
+    creator_image:
+      "https://yt3.ggpht.com/VRZSo7NrS60V6RAHiISfGnkWjkOCy9aznpDA4_45TuRf7CVnyyeNeyS9aVSvkUJa1giWqhmJVw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/YrRlwUw0GY4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDSDxmhsDg4v1WyeYHSCUt_7kPV1Q",
+    description:
+      "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
+    views: "3.3M",
+    creator: "INTER",
+    category: "GTA V",
+  },
+  // top 10 mods gta
+  {
+    _id: "BAphrQJjg0Q",
+    title: "GTA 5 - Best Mods!",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLQJP0MjR9YohzlKywbMVpEcnTBNHRAxziO4oxn6jw=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/BAphrQJjg0Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBeIunH-nwuYFFoYg2eOGKrkDkR1Q",
+    description:
+      "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
+    views: "400k",
+    creator: "Madd Trevor",
+    category: "GTA V",
+  },
+  {
+    _id: "Y_amW5BOu7M",
+    title: "GTA 5 - MEGA Tsunami FLOODS Los Santos!",
+    creator_image:
+      "https://yt3.ggpht.com/ytc/AKedOLQZ-N7hUNR7mzBImO-_3hIbZJbs7LuQUSrYwnVMQg=s68-c-k-c0x00ffffff-no-rj",
+    thumbnail:
+      "https://i.ytimg.com/vi/Y_amW5BOu7M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBnsLVeiyWyeoGu7nAn8d6jUFIevQ",
+    description:
+      "Check out the first look at Battlefield 2042 official pre-alpha gameplay!  Battlefield™ 2042 is available on November 19, 2021, with early access starting November 12, ",
+    views: "7.5M",
+    creator: "Nought",
+    category: "GTA V",
   },
 ];
